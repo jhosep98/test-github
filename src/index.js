771071bd666835336370d47cdf1a1a305c75f104
jhosep98 <jhosep149@hotmail.com>
2021-02-28 21:19:27 -0300

    change port server

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+const PORT = 3000;
+
 app.get("/", (req, res) => res.send("Hello World"));
 
-app.listen(3000, () => console.log("server on port: 3000"));
+app.listen(PORT, () => console.log("server on port:", PORT));
